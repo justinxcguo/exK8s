e9019acfde9e9601fdb1f23683e5ea72ee0446ec
justin guo <justinxcguo@gmail.com>
2019-10-20 22:36:15 +0200

    fix start error

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    if(this.state.seenIndexes)
+    if(this.state.seenIndexes.length > 0)
       return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
